4ae3b1c4e24f2049f0e5edf0db90129ef44ca241
MarcoLGP <42444188+MarcoLGP@users.noreply.github.com>
2023-03-06 00:54:19 -0300

    Update app.py

--- app.py
+++ app.py
@@ -22,4 +22,4 @@ def getScore():
     return jsonify(ml_client_bank_instance.getMlScore())
 
 if __name__ == "__main__":
-    app.run(debug=True, port=5000)
+    app.run(debug=False, host="0.0.0.0")
